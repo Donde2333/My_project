c2adb653355ed477d54da74819051eced7d83663
Yan <918357131@qq.com>
2025-06-04 21:10:59 +0800

    sorted函数+filter函数

--- demo/test10-函数练习.py
+++ demo/test10-函数练习.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# 把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# 把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。 map函数
 s1 = ['adam', 'LISA', 'barT']
 
 
@@ -10,3 +10,15 @@ def normalize(name):
 
 s2 = list(map(normalize, s1))
 print(s2)
+
+
+# 假设我们用一组tuple表示学生名字和成绩 sorted函数
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+L2 = sorted(L, key=by_name)
+print("按名字排序:", L2)
--- demo/test9-函数学习.py
+++ demo/test9-函数学习.py
@@ -38,3 +38,26 @@ def char2num(s):
 r6 = reduce(fn, map(char2num, '13579'))
 
 print(r6)
+
+
+# 检查一个整数是否是回数 filter函数
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]  # 将数字转换为字符串后比较正反序
+
+
+palindromes = list(filter(is_palindrome, range(100)))
+print(palindromes)
+
+
+# 排序 sorted函数 key=条件函数 reverse=
+s3 = sorted([36, 5, -12, 9, -21])
+print("排序:", s3)
+
+s4 = sorted([36, 5, -12, 9, -21], key=abs)
+print("绝对值排序：", s4)
+
+name2 = ['bob', 'about', 'Zoo', 'Credit']
+s5 = sorted(name2, key=str.lower)
+s6 = sorted(name2, key=str.lower, reverse=True)
+print(s5)
+print(s6)
